87c60f6fb7f42c4bb29e473a3beb635d54e5dfc7
morgan <morgan.malavsi@gmail.com>
2019-06-23 13:17:45 +0200

    final

--- index.js
+++ index.js
@@ -1,53 +1,87 @@
 const express = require('express');
-const upload = require('express-fileupload');
 const path = require('path');
-const okrabyte = require('okrabyte');
-const fs = require("fs");
 const tesseract = require('tesseract.js');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const ejs = require('ejs');
 
+// set storage engine 
+const storage = multer.diskStorage({
+    destination: './public/', 
+    filename: function(req, file, cb){
+        cb(null,file.fieldname + '-' + Date.now() + 
+        path.extname(file.originalname));
+    }
+});
+
+// Init Upload 
+// set limit and extension 
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 5000000},
+    fileFilter: function(req, file, cb){
+        checkFileType(file,cb);
+    }
+}).single('myImage');
+
+function checkFileType(file,cb){
+    // check extension 
+    const filetypes = /jpeg|jpg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // check mimetype 
+    const mimetype = filetypes.test(file.mimetype);
+    if (extname && mimetype)
+        return cb(null, true);
+    else 
+        return cb('error: Images only');
+}
+
+// tesseract engine
 const { TesseractWorker } = tesseract;
 const worker = new TesseractWorker();
 
-var app = express();
-app.use(upload());
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(bodyParser.json());
 
-
-// GET MOTHODS
+// GET METHODS
 // TODO: sostituire il ridirezionamento della pagina con quella vera 
 app.get('/', (req,res) => {
-    res.sendFile('/Users/morgan/Desktop/JPGtoTXTwebApp/start.html');
+    res.render('start');
 });
 
+app.post('/upload', (req,res) => {
+    upload(req,res,(err) => {
+        if (err){
+            res.render('start', {
+                msg: err
+            });
+        } else {
+            if (req.file == undefined){
+                res.render('start', {
+                    msg: 'Error: no File Selected'
+                });
+            } else {
 
-// POST METHODS
-app.post('/upload', (req, res) => {
-    if (req.files){
-        console.log(req.files);
-        // get extension if the file
-        let file = req.files.filename;
-        let extension = path.extname(file.name);
-        // if the file is an IMAGE (.jpg || .jpeg || .png) then is accepted
-        // while if it is a different file return 
-        let extensionLowerCase = extension.toLowerCase();
-        if (extensionLowerCase === '.jpg' || extensionLowerCase === '.jpeg'){
-            // code for the image
-            worker
-                .recognize('./images/hello_world.png')
+                worker
+                .recognize('./public/'+req.file.filename)
                 .progress((p) => {
                     console.log('progress', p);
                 })
                 .then(({ text }) => {
-                    console.log(text);
+                    res.render('start', {
+                        msg: 'File Uploaded',
+                        file: req.file.filename,
+                        text: text
+                    });
+                    console.log(text)
                     worker.terminate();
                 });
-        } else {
-            // throw exception is not an image     
+            }
         }
-        
-        res.sendStatus(200);
-    }
+    });
 });
-
 
 var port = process.env.PORT || 8080;
 app.listen(port);
